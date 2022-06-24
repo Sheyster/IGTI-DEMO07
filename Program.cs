2663ac8766f376d97e5c03c79b4bac266ac8f98f
I4daa5s5I3EszH9OpOV4W10rY52cLndn6NBjcIZm3zY= <elton.ferreira@nava.com.br>
2022-06-24 16:27:44 -0300

    version 1.0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IGTI_DEMO07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
